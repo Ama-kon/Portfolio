9d5b7b6297d86bcdb99213d47d789f54eade7c63
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-07-22 19:46:02 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { AtfComponent } from './atf/atf.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: AtfComponent }];
