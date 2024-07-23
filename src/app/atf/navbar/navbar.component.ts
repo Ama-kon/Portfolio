3a47080ba35ca2a497703f1bdf1a91fa4a3d352b
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-07-23 16:12:45 +0200

    created navbar component & basic structure & style

--- src/app/atf/navbar/navbar.component.ts
+++ src/app/atf/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
