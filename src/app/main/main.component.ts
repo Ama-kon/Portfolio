2fb179a32ced2cb221bb406b49a8467682c0ea2b
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-07-26 01:06:11 +0200

    components now in main.component

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AtfComponent } from './atf/atf.component';
+import { NavbarComponent } from './atf/navbar/navbar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [AtfComponent, NavbarComponent, AboutMeComponent, SkillSetComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+})
+export class MainComponent {}
