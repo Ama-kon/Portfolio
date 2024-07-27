1db7f2a8d046718b5b6dd707346dfb48f75a92a7
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-07-27 02:43:26 +0200

    my work component + style & nested projects component

---
+++ src/app/main/my-work/my-work.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ProjectsComponent } from './projects/projects.component';
+
+@Component({
+  selector: 'app-my-work',
+  standalone: true,
+  imports: [ProjectsComponent],
+  templateUrl: './my-work.component.html',
+  styleUrl: './my-work.component.scss',
+})
+export class MyWorkComponent {}
