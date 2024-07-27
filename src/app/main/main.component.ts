f6598df92191a97b1af6bc13cf1220aeec1b8ccf
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-07-27 02:42:03 +0200

    added skill set & my work component & adjusted their style

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,11 +3,18 @@ import { AtfComponent } from './atf/atf.component';
 import { NavbarComponent } from './atf/navbar/navbar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
+import { MyWorkComponent } from './my-work/my-work.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [AtfComponent, NavbarComponent, AboutMeComponent, SkillSetComponent],
+  imports: [
+    AtfComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    SkillSetComponent,
+    MyWorkComponent,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
