ac6f4d869ad98a4cd9655c3e81a4eccd98db4d36
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-07-29 13:44:12 +0200

    new testimonials component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from './atf/navbar/navbar.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { MyWorkComponent } from './my-work/my-work.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @Component({
   selector: 'app-main',
@@ -14,6 +15,7 @@ import { MyWorkComponent } from './my-work/my-work.component';
     AboutMeComponent,
     SkillSetComponent,
     MyWorkComponent,
+    TestimonialsComponent,
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
