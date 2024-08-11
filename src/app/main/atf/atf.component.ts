fdeb1e94d0804acfb53c076a5f4e49ab73f094fb
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-08-12 02:08:03 +0300

    change of img / links for buttons/ conection with contact component

--- src/app/main/atf/atf.component.ts
+++ src/app/main/atf/atf.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-atf',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ContactComponent],
   templateUrl: './atf.component.html',
   styleUrl: './atf.component.scss',
 })
