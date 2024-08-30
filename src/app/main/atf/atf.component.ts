c71472cb4a7c3f4348b66446bff9a7a37b11e578
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-08-30 03:26:31 +0300

    added TranslateModule &/OR TranslateService

--- src/app/main/atf/atf.component.ts
+++ src/app/main/atf/atf.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContactComponent } from '../contact/contact.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-atf',
   standalone: true,
-  imports: [NavbarComponent, ContactComponent],
+  imports: [NavbarComponent, ContactComponent, TranslateModule],
   templateUrl: './atf.component.html',
   styleUrl: './atf.component.scss',
 })
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  constructor(public translateService: TranslateService) {}
   window = window;
 }
