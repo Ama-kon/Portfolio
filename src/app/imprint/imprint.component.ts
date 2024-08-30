18d9fdee3aad466d2b2d03098f3d4263f2a00f24
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-08-30 03:23:37 +0300

    func to change to selected language

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
-export class ImprintComponent {}
+export class ImprintComponent implements OnInit {
+  constructor(
+    private translateService: TranslateService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      const lang = params['lang'];
+      if (lang) {
+        this.translateService.use(lang);
+      }
+    });
+  }
+}
