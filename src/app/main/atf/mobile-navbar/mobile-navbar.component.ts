4487a5237f07f7b089c0c5f7afc86782cbc282c6
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-08-30 03:27:03 +0300

    Switch language function / added TranslateModule & TranslateService

--- src/app/main/atf/mobile-navbar/mobile-navbar.component.ts
+++ src/app/main/atf/mobile-navbar/mobile-navbar.component.ts
@@ -1,21 +1,28 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ToggleMenuService } from '../../../services/toggle-menu.service';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-mobile-navbar',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, TranslateModule],
   templateUrl: './mobile-navbar.component.html',
   styleUrl: './mobile-navbar.component.scss',
 })
 export class MobileNavbarComponent {
-  constructor(private toggleMenuService: ToggleMenuService) {}
+  constructor(
+    private toggleMenuService: ToggleMenuService,
+    private translate: TranslateService
+  ) {}
   @Input() isMobileMenuOpen: boolean = false;
   @Output() menuToggled = new EventEmitter<boolean>();
   closeMobileNav() {
     this.toggleMenuService.toggleMenu();
     this.toggleMenuService.onMenuToggled(this.isMobileMenuOpen);
     this.menuToggled.emit(this.toggleMenuService.isMobileMenuOpen);
+  }
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
   }
 }
--- src/app/main/atf/navbar/navbar.component.ts
+++ src/app/main/atf/navbar/navbar.component.ts
@@ -3,20 +3,32 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MobileNavbarComponent } from '../mobile-navbar/mobile-navbar.component';
 import { ToggleMenuService } from '../../../services/toggle-menu.service';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MobileNavbarComponent],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MobileNavbarComponent,
+    TranslateModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   @Input() isMobileMenuOpen: boolean = false;
   @Output() menuToggled = new EventEmitter<boolean>();
-  constructor(private toggleMenuService: ToggleMenuService) {}
+  constructor(
+    private toggleMenuService: ToggleMenuService,
+    private translate: TranslateService
+  ) {}
   toggleMobile() {
     this.toggleMenuService.toggleMenu();
     this.menuToggled.emit(this.toggleMenuService.isMobileMenuOpen);
+  }
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
   }
 }
