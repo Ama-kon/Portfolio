9540f5ed82aadb2a7a3ea2eddd2cbae863952e2a
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-06 01:11:40 +0300

    added navbar to app.comp. &  removed from main comp.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './main/atf/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { MobileNavbarComponent } from './main/atf/mobile-navbar/mobile-navbar.component';
+import { ToggleMenuService } from './services/toggle-menu.service';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    MainComponent,
+    FooterComponent,
+    NavbarComponent,
+    MobileNavbarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  isMobileMenuOpen: boolean = false;
+  constructor(private toggleMenuService: ToggleMenuService) {}
+
+  ngOnInit() {
+    this.isMobileMenuOpen = this.toggleMenuService.isMobileMenuOpen;
+  }
+
+  ngAfterViewInit() {
+    AOS.init();
+    this.preventDrag();
+  }
+
+  onToggled(isOpen: boolean) {
+    this.toggleMenuService.onMenuToggled(isOpen);
+    this.isMobileMenuOpen = isOpen;
+  }
+
+  preventDrag() {
+    let images = document.querySelectorAll('img');
+    let links = document.querySelectorAll('a');
+    images.forEach((img) => {
+      img.addEventListener('dragstart', (e) => e.preventDefault());
+    });
+    links.forEach((link) => {
+      link.addEventListener('dragstart', (e) => e.preventDefault());
+    });
+  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,7 +8,6 @@ import { MyWorkComponent } from './my-work/my-work.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from '../shared/footer/footer.component';
-import { ToggleMenuService } from '../services/toggle-menu.service';
 
 @Component({
   selector: 'app-main',
@@ -27,31 +26,4 @@ import { ToggleMenuService } from '../services/toggle-menu.service';
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent implements OnInit {
-  isMobileMenuOpen: boolean = false;
-  constructor(private toggleMenuService: ToggleMenuService) {}
-
-  ngOnInit() {
-    this.isMobileMenuOpen = this.toggleMenuService.isMobileMenuOpen;
-  }
-
-  ngAfterViewInit() {
-    this.preventDrag();
-  }
-
-  onToggled(isOpen: boolean) {
-    this.toggleMenuService.onMenuToggled(isOpen);
-    this.isMobileMenuOpen = isOpen;
-  }
-
-  preventDrag() {
-    let images = document.querySelectorAll('img');
-    let links = document.querySelectorAll('a');
-    images.forEach((img) => {
-      img.addEventListener('dragstart', (e) => e.preventDefault());
-    });
-    links.forEach((link) => {
-      link.addEventListener('dragstart', (e) => e.preventDefault());
-    });
-  }
-}
+export class MainComponent {}
